eddaa72ec1f1284d85cdfec8ce15b88f05e4409b
Michael Ruocco <michael.ruocco@hotmail.com>
2022-10-05 20:36:26 +0100

    uses paramterized test to fix sonar code smell

--- src/test/java/uk/co/mruoc/country/CountryCodeFormatterTest.java
+++ src/test/java/uk/co/mruoc/country/CountryCodeFormatterTest.java
@@ -3,6 +3,8 @@ package uk.co.mruoc.country;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class CountryCodeFormatterTest {
 
@@ -17,46 +19,9 @@ class CountryCodeFormatterTest {
         assertThat(formatted).isNull();
     }
 
-    @Test
-    void shouldReturnEmptyStringUnchanged() {
-        String input = "";
-
-        String formatted = formatter.to2AlphaIfPossible(input);
-
-        assertThat(formatted).isEmpty();
-    }
-
-    @Test
-    void shouldReturnAnyStringLongerThan3Unchanged() {
-        String input = "ABCD";
-
-        String formatted = formatter.to2AlphaIfPossible(input);
-
-        assertThat(formatted).isEqualTo(input);
-    }
-
-    @Test
-    void shouldReturnAnyStringShorterThan2Unchanged() {
-        String input = "A";
-
-        String formatted = formatter.to2AlphaIfPossible(input);
-
-        assertThat(formatted).isEqualTo(input);
-    }
-
-    @Test
-    void shouldReturnInvalid2AlphaCountryCodeUnchanged() {
-        String input = "AB";
-
-        String formatted = formatter.to2AlphaIfPossible(input);
-
-        assertThat(formatted).isEqualTo(input);
-    }
-
-    @Test
-    void shouldReturn2AlphaCountryCodeUnchanged() {
-        String input = "GB";
-
+    @ParameterizedTest
+    @ValueSource(strings = {"ABCD", "A", "AB", "GB", ""})
+    void shouldReturnInputUnchanged(String input) {
         String formatted = formatter.to2AlphaIfPossible(input);
 
         assertThat(formatted).isEqualTo(input);
